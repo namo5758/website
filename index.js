121a418f2f51fc5762c3d1074d33b3224f5b998f
namo5758 <76212150+namo5758@users.noreply.github.com>
2022-06-05 17:15:20 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const hbs = require('hbs');
+const app = express();
+// กำหนดหน้าที่จะโชว์
+const HomeRounter = require('./routers/Home');
+const StoreRounter = require('./routers/store');
+const adminloginRounter = require('./routers/adminlogin');
+const logoutRounter = require('./routers/logout');
+const adminRounter = require('./routers/admin');
+const loginRounter = require('./routers/login');
+const registerRounter = require('./routers/register');
+//config เฉยไม่มีอะไร
+const dotenv = require("dotenv").config();
+const port = process.env.APP_PORT; 
+const session = require("express-session");
+//เกี่ยวกับ cookie
+app.use(session({
+	secret: 'namo5758',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(session({
+	secret: 'namo5758',
+	resave: true,
+	saveUninitialized: true
+}));
+// ทำให้รับค่าจาก form ได้
+app.use(express.urlencoded({ extended: true }));
+// เอาไว้โชว์หน้าhtml
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + '/views/partials');
+app.use('/static', express.static('static'));
+// กำหนดหน้าที่จะโชว์
+app.use('/', HomeRounter);
+app.use('/', StoreRounter);
+app.use('/', adminloginRounter);
+app.use('/', logoutRounter);
+app.use('/', adminRounter);
+app.use('/', loginRounter);
+app.use('/', registerRounter);
+app.listen(port, () => {
+    console.log(`รันเว็ปละ http://localhost:${port}`);
+});
